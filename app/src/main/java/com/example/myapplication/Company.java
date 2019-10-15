c4efe4a3aaefe87c0ea1e171df5c6092226640a5
tanuj <saraft7707@gmail.com>
2019-10-15 19:22:29 +0530

    added Company Profile

--- app/src/main/java/com/example/myapplication/Company.java
+++ app/src/main/java/com/example/myapplication/Company.java
@@ -7,12 +7,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.myapplication.Profile.CompanyData;
 import com.example.myapplication.Profile.Profile;
+import com.example.myapplication.String.String_method;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,17 +34,41 @@ public class Company extends AppCompatActivity {
     ArrayAdapter adapter;
     String [] user={"abcd","b","c","d"};
     TextView textView;
+    EditText e1,e2,e3,e4;
+    Button b1;
     ListView listView;
     Map<CompanyData, Profile> data = new HashMap<CompanyData,Profile>();
     ArrayList value ;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company);
-         listView=(ListView)findViewById(R.id.listView);
+        // listView=(ListView)findViewById(R.id.listview);
         textView =(TextView)findViewById(R.id.textView);
+        e1=(EditText)findViewById(R.id.companyname) ;
+        e2=(EditText)findViewById(R.id.companyaddress) ;
+        e3=(EditText)findViewById(R.id.companyctc) ;
+        e4=(EditText)findViewById(R.id.companyphone) ;
+        database =FirebaseDatabase.getInstance();
+        myref= database.getReference("Company");
 
+        b1=(Button) findViewById(R.id.Add_Company);
+        b1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String cname= e1.getText().toString();
+                String caddress= e2.getText().toString();
+                String cctc= e3.getText().toString();
+                String cphone= e4.getText().toString();
+                CompanyData c=new CompanyData(cname,caddress,cphone,"60%",Float.valueOf(cctc));
+
+               myref.child("60%").setValue(c);
+            }
+        });
         database =FirebaseDatabase.getInstance();
         myref= database.getReference("Users");
         //adapter=new ArrayAdapter(getApplicationContext(),R.layout.listlayout,R.id.textView,data);
@@ -48,6 +77,7 @@ public class Company extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
+
 
                 data = (Map)dataSnapshot.getValue();
 

46d69dbf41376837e60a0df796c754bdf705feb7
Prasad Mandge <p.mandge212@gmail.com>
2019-10-16 12:07:01 +0530

    show company list

---
+++ app/src/main/java/com/example/myapplication/DataViewHolder.java
@@ -0,0 +1,7 @@
+package com.example.myapplication;
+
+import android.widget.TextView;
+
+public class DataViewHolder   {
+    TextView t1;
+}
---
+++ app/src/main/java/com/example/myapplication/ListCompany.java
@@ -0,0 +1,82 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.myapplication.Profile.CompanyData;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ListCompany extends AppCompatActivity {
+    FirebaseDatabase database;
+    FirebaseUser user;
+    FirebaseAuth mauth;
+    DatabaseReference myref;
+    ListView l1;
+    ArrayList<String> name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_company);
+        database = FirebaseDatabase.getInstance();
+        myref = database.getReference("Company");
+        l1 = (ListView) findViewById(R.id.company_id);
+        Query query = myref;
+        name =new ArrayList<String>();
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_view_layout, R.id.companyname_id, name);
+        l1 = (ListView) findViewById(R.id.company_id);
+        //l1.setAdapter(arrayAdapter);
+
+        myref.child("60").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String cname = dataSnapshot.getKey();
+                name.add(String.valueOf(cname));
+                l1.setAdapter(arrayAdapter);
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
